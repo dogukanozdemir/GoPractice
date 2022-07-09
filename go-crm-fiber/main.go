6dbeaa7ac80631b6a6549a5433be1b15bddf7580
Dogukan <dogu757@gmail.com>
2022-07-09 18:29:06 +0300

    crm done but has errors, check it

---
+++ go-crm-fiber/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dogukanozdemir/go-crm-fiber/database"
+	"github.com/dogukanozdemir/go-crm-fiber/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("Failed to connect to DB")
+	}
+
+	fmt.Println("Connected to SQLITE!")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
+}
