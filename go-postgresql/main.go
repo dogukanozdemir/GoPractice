db3024ce9cc22ad6bf1d5b643c879e76006b7cbb
dogukan <dogu757@gmail.com>
2022-07-15 03:43:13 +0300

    fixed env issue

--- go-postgresql/main.go
+++ go-postgresql/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main(){
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading environment file!")
+	}
 
-	connStr := "postgres://juomnbnofveahe:51df9706d98feab9b4f75e825c5deb916a546ce1584146c2035b37718632e99f@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d2u256bd7288o7"
-	db, err := sql.Open("postgres", connStr)
+	postgres_uri := os.Getenv("POSTGRES_URI")
+	
+	db, err := sql.Open("postgres", postgres_uri)
 	if err != nil {
 		panic(err)
 	}
