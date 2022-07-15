e8dda0b8f203fc219f3847c1781f75d7b1c06d9d
Dogukan <dogu757@gmail.com>
2022-07-15 17:08:23 +0300

    fixed uri issue

---
+++ go-postgresql/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading environment file!")
+	}
+
+	postgres_uri := os.Getenv("POSTGRES_URI")
+
+	db, err := sql.Open("postgres", postgres_uri)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+	defer db.Close()
+
+}
